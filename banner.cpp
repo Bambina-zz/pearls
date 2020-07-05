318e39c2674784aee2c4a4d7087942de670a4cf1
Mayuko Hirono <mayuko.hirono@gmail.com>
2020-07-05 12:31:53 +0900

    User can input C too

--- banner.cpp
+++ banner.cpp
@@ -2,49 +2,91 @@
 using namespace std;
 
 /** s-in
+ * C
+ */
+
+/** s-out
+ *   XXXXX
+ *  XX   XX
+ * XX      
+ * XX
+ * XX      
+ *  XX   XX
+ *   XXXXX
+ */
+
+/** s-in
  * I
  */
 
 /** s-out
  * XXXXXXXXX
  * XXXXXXXXX
- * XXXXXXXXX
  *    XXX   
  *    XXX   
  *    XXX   
- * XXXXXXXXX
  * XXXXXXXXX
  * XXXXXXXXX
  */
 
+string trim(string inputText) {
+  string format = "";
+  for (unsigned int i = 0; i < inputText.length(); i++){
+    if (inputText[i] == 'C' || inputText[i] == 'I') {
+      format += inputText[i];
+    }
+  }
+  return format;
+}
+
 int main() {
   string input;
   cin >> input;
-  if (input != "I") {
+  string format = trim(input);
+  if (format.length() == 0) {
+    cout << "Input C or I";
     return 0;
   }
-  string data_i[3][8] = {{"3","1","9","X"},
-                  {"3","3","3","b","3","X","3","b"},
-                  {"3","1","9","X"}};
-  
-  for (int i = 0; i < 3; i++) {
-    string item[8] = data_i[i];
-    int num_lines = stoi(item[0]);
-    int num_patterns = stoi(item[1]);
-    string text;
-    for (int j = 0; j < num_patterns; j++) {
-      int num_chars = stoi(item[j*2+2]);
-      string c = item[j*2+3];
-      for (int k = 0; k < num_chars; k++) {
-        if (c == "b") {
-          text += " ";
-        } else {
-          text += c;
+
+  array<array<string, 10>, 5> data_c{{
+    {"1","3","2","b","5","X","1","b"},
+    {"1","4","1","b","2","X","3","b","2","X"},
+    {"3","2","2","x","6","b"},
+    {"1","4","1","b","2","X","3","b","2","X"},
+    {"1","3","2","b","5","X","1","b"}}};
+  array<array<string, 10>, 5> data_i{{
+    {"2","1","9","X"},
+    {"3","3","3","b","3","X","3","b"},
+    {"2","1","9","X"},
+    {"0","0"},
+    {"0","0"}}};
+
+  map<char, array<array<string, 10>, 5>*> map;
+  map['C'] = &data_c;
+  map['I'] = &data_i;
+
+  for(unsigned int i = 0; i < format.length(); i++) {
+    array<array<string, 10>, 5> target_data = *map[format[i]];
+    int S = target_data.size();
+    for(int j = 0; j < S; j++) {
+      int num_lines = stoi(target_data[j][0]);
+      int num_patterns = stoi(target_data[j][1]);
+      string text;
+      for(int k = 0; k < num_patterns; k++) {
+        int num_repeat = stoi(target_data[j][k*2+2]);
+        string target_char = target_data[j][k*2+3];
+        for (int l = 0; l < num_repeat; l++) {
+          if (target_char == "b") {
+            text += " ";
+          } else {
+            text += target_char;
+          }
         }
       }
+      for(int m = 0; m < num_lines; m++) {
+        cout << text << endl;
+      }
     }
-    for (int l = 0; l < num_lines; l++) {
-      cout << text << endl;
-    }
+    cout << endl;
   }
 }
